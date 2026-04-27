96f638d91258b752bd1c74fc00c7805f773eec54
Miguel Rugerio <miguel.rugerio@outlook.com>
2026-04-27 09:18:47 -0600

    Agregar módulo bloquesTexto con ejercicio de text blocks
    - v0/Principal: uso de text blocks (\"\"\" ... \"\"\") introducidos en
      Java 15, con escape de comillas internas y continuación de línea
      mediante backslash al final de línea

---
+++ bloquesTexto/src/com/curso/v0/Principal.java
@@ -0,0 +1,16 @@
+package com.curso.v0;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		String cadena = """
+				aaaaaaaaaaaaaaaaaaa
+				bbbbbbb\"bbbbbbb\"bbbbb
+				ccccccccccccccccccc\
+				ddddddddddddddddddd
+				""";
+
+		System.out.println(cadena);
+	}
+
+}
